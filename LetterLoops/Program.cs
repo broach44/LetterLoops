b68473a607fc13e9455501af2442c4b7f80a8e53
Crystal Broach <crysland@gmail.com>
2020-02-27 21:16:40 -0600

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var finalString = "";
+
+            Console.WriteLine("Enter a random string:");
+            var userEntry = Console.ReadLine();
+
+            for (int i = 1; i <= userEntry.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {                           
+                    if (j == 0)
+                    {
+                        var upperString = userEntry.ToUpper();
+                        finalString += upperString[i - 1];
+                    }
+                    else
+                    finalString += userEntry[i-1];
+                }
+                if (i != userEntry.Length)
+                {
+                    finalString += "-";
+                }
+            }
+
+            Console.WriteLine($"Output is: {finalString}");
+            Console.ReadLine();
+        }
+    }
+}
